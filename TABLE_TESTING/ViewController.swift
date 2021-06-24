f67c443f308144ffb598ee78cd3b07458f860182
MUJTABA <kmujtaba754@gmail.com>
2021-06-24 14:50:14 +0500

    Initial Commit

---
+++ TABLE_TESTING/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TABLE_TESTING
+//
+//  Created by MUJTABA KHAN on 24/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
